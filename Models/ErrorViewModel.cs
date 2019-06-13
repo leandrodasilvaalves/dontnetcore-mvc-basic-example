8ab082f26b2519cfc4dc741b8546ea4c32be9ced
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2019-06-13 17:01:27 -0300

    Carga inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace teste_redhat_openshift.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
